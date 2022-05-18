569144082bd8387a618cf6bc331e1ef1d7d814e7
eeue56 <enalicho@gmail.com>
2022-05-19 01:22:44 +0200

    add game of life

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ derw install
 derw bundle --entry src/AfterTime.derw --output dist/aftertime.js
 derw bundle --entry src/Button.derw --output dist/button.js
 derw bundle --entry src/Duality.derw --output dist/duality.js
+derw bundle --entry src/GameOfLife.derw --output dist/gameoflife.js
 derw bundle --entry src/Jeopardy.derw --output dist/jeopardy.js
 derw bundle --entry src/TextInput.derw --output dist/textinput.js
